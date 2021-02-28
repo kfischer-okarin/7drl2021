024dd89e2d18ec8a8cfc693c40f9bdf9908c5cf2
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2021-02-28 18:43:58 +0900

    Rewrite to shadow-casting-like

--- game/app/field_of_view.rb
+++ game/app/field_of_view.rb
@@ -5,20 +5,31 @@ class FieldOfView
     @map = map
     @from = from
     @area = area
+    @obstacles = sort_by_distance(map.blocking_positions_in(@area))
     @visible_positions = calc_visible_positions
   end
 
   private
 
-  def visible?(position)
-    true
+  def covered_positions(obstacle)
+    []
   end
 
   def calc_visible_positions
-    Set.new.tap { |result|
-      @area.each_position do |position|
-        result << position if visible?(position)
+    Set.new(*@area.each_position).tap { |result|
+      @obstacles.each do |obstacle|
+        next unless result.include? obstacle
+
+        covered_positions(obstacle).each do |covered_position|
+          result.delete covered_position
+        end
       end
+    }
+  end
+
+  def sort_by_distance(positions)
+    positions.sort_by { |position|
+      [(@from.x - position.x).abs, (@from.y - position.y).abs].min
     }
   end
 end
