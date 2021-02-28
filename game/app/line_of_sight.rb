220ff5e3b0194bcf42738b4d92a025d7559d7025
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2021-02-28 16:32:13 +0900

    Add horizontal/vertical line intersections

--- game/app/line_of_sight.rb
+++ game/app/line_of_sight.rb
@@ -8,6 +8,48 @@ class LineOfSight
     @visible_positions = calc_visible_positions
   end
 
+  class HorizontalLine
+    attr_reader :y, :x1, :x2
+
+    def initialize(x1, x2, y:)
+      @x1 = x1
+      @x2 = x2
+      @y = y
+    end
+
+    def intersection_with(other)
+      return @y == other.y ? :identical : nil if other.is_a? HorizontalLine
+      return intersection_with_vertical_line(other) if other.is_a? VerticalLine
+    end
+
+    private
+
+    def intersection_with_vertical_line(other)
+      (@y - other.y1) / (other.y2 - other.y1)
+    end
+  end
+
+  class VerticalLine
+    attr_reader :x, :y1, :y2
+
+    def initialize(y1, y2, x:)
+      @y1 = y1
+      @y2 = y2
+      @x = x
+    end
+
+    def intersection_with(other)
+      return @x == other.x ? :identical : nil if other.is_a? VerticalLine
+      return intersection_with_horizontal_line(other) if other.is_a? HorizontalLine
+    end
+
+    private
+
+    def intersection_with_horizontal_line(other)
+      (@x - other.x1) / (other.x2 - other.x1)
+    end
+  end
+
   private
 
   def visible?(position)
--- game/tests/app/line_of_sight.rb
+++ game/tests/app/line_of_sight.rb
@@ -27,5 +27,49 @@ def test_line_of_sight_without_obstacle(_args, assert)
   )
 end
 
+# Line intersection tests
+
+def test_horizontal_line_parallel_to_horizontal_line(_args, assert)
+  line1 = LineOfSight::HorizontalLine.new(0, 1, y: 0)
+  line2 = LineOfSight::HorizontalLine.new(0, 1, y: 1)
+
+  assert.nil! line1.intersection_with(line2)
+end
+
+def test_horizontal_line_identical_to_horizontal_line(_args, assert)
+  line1 = LineOfSight::HorizontalLine.new(0, 1, y: 0)
+  line2 = LineOfSight::HorizontalLine.new(0, 1, y: 0)
+
+  assert.equal! line1.intersection_with(line2), :identical
+end
+
+def test_vertical_line_parallel_to_vertical_line(_args, assert)
+  line1 = LineOfSight::VerticalLine.new(0, 1, x: 0)
+  line2 = LineOfSight::VerticalLine.new(0, 1, x: 1)
+
+  assert.nil! line1.intersection_with(line2)
+end
+
+def test_vertical_line_identical_to_vertical_line(_args, assert)
+  line1 = LineOfSight::VerticalLine.new(0, 1, x: 0)
+  line2 = LineOfSight::VerticalLine.new(0, 1, x: 0)
+
+  assert.equal! line1.intersection_with(line2), :identical
+end
+
+def test_horizontal_line_intersect_vertical_line(_args, assert)
+  line1 = LineOfSight::HorizontalLine.new(0, 1, y: 10)
+  line2 = LineOfSight::VerticalLine.new(0, 2, x: 5)
+
+  assert.equal! line1.intersection_with(line2), 5
+end
+
+def test_vertical_line_intersect_horizontal_line(_args, assert)
+  line1 = LineOfSight::VerticalLine.new(0, 1, x: -3)
+  line2 = LineOfSight::HorizontalLine.new(0, 2, y: 5)
+
+  assert.equal! line1.intersection_with(line2), -1.5
+end
+
 $gtk.reset 100
 $gtk.log_level = :off
